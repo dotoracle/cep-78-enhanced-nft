7db98d9bd46d94dde4cce6894c26c9729ee067a2
tonititi <dong.vu.cong@gmail.com>
2023-06-16 17:06:36 +0700

    add .gitignore

--- scripts2/approveToUnlock.js
+++ scripts2/approveToUnlock.js
@@ -16,6 +16,7 @@ let publicKey = Keys.Secp256K1.privateToPublicKey(Uint8Array.from(privateKeyBuff
 let KEYS = new Keys.Secp256K1.parseKeyPair(publicKey, Uint8Array.from(privateKeyBuffer), 'raw')
 
 const test = async () => {
+  console.log("test wrapped nft")
   const nftContractHash = await CWeb3.Contract.getActiveContractHash(nftPackageHash, CHAIN_NAME)
   const contract = await CWeb3.Contract.createInstanceWithRemoteABI(nftContractHash, NODE_ADDRESS, CHAIN_NAME)
   const metadata = JSON.stringify({
@@ -24,14 +25,14 @@ const test = async () => {
     checksum: "940bffb3f2bba35f84313aa26da09ece3ad47045c6a1292c2bbd2df4ab1a55fb"
   })
   const hash = await contract.contractCalls.approveToClaim.makeDeployAndSend({
-      keys: KEYS,
-      args: {
-        mintId: `0x7788d03de297137446ae4d66a5630d40064e8ec398305c7189f717e4b41914e5-43113-96945816564243-94-${"0xEC83b9Dd29D22D53870AFC9223689294dC2153d1".toLowerCase()}-43113`,
-        tokenOwner: KEYS.publicKey,
-        tokenMetaDatas: configed.tokenIds.map(e => metadata),
-        tokenIds: configed.tokenIds,
-      },
-      paymentAmount: '10000000000'
+    keys: KEYS,
+    args: {
+      mintId: `0x7788d03de297137446ae4d66a5630d40064e8ec398305c7189f717e4b41914e5-43113-96945816564243-94-${"0xEC83b9Dd29D22D53870AFC9223689294dC2153d1".toLowerCase()}-43113`,
+      tokenOwner: KEYS.publicKey,
+      tokenMetaDatas: configed.tokenIds.map(e => metadata),
+      tokenIds: configed.tokenIds,
+    },
+    paymentAmount: '10000000000'
   })
 
   console.log(`... Contract installation deployHash: ${hash}`);
